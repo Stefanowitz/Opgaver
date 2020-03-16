ae203a6d93f3c543d2e43bf60c02817c89dc6a2e
Stefanowitz <40226568+Stefanowitz@users.noreply.github.com>
2020-03-16 14:11:16 +0100

    Update

--- Calcolator/Calcolator/Program.cs
+++ Calcolator/Calcolator/Program.cs
@@ -4,10 +4,10 @@ namespace Calculator
 {
     class Program
     {
-        static void Main(string[] args)
+        int number1 = 0; int number2 = 0;
+        public void Main(string[] args)
         {
             Begining:
-            int number1 = 0; int number2 = 0;
 
             Console.WriteLine("Hello im your trusty Calcolator\r");
             Console.WriteLine("------------------------\n");
@@ -18,8 +18,38 @@ namespace Calculator
             Console.WriteLine("Type another number and press Enter");
             number2 = Convert.ToInt32(Console.ReadLine());
 
+            Console.WriteLine("Please Choose an option and watch the magic happen:");
+            Console.WriteLine("\ta - Add");
+            Console.WriteLine("\ts - Subtract");
+            Console.WriteLine("\tm - Multiply");
+            Console.WriteLine("\td - Divide");
+            Console.Write("Your option?");
             Math();
 
+            MoreMathMagic();
+        }
+        public void Math()
+        {
+
+            switch (Console.ReadLine())
+            {
+                case "a":
+                    Console.WriteLine($"Your result: {number1} + {number2} (Insert Drum roll here)= " + (number1 + number2));
+                    break;
+                case "s":
+                    Console.WriteLine($"Your result: {number1} - {number2} (Insert Drum roll here)= " + (number1 - number2));
+                    break;
+                case "m":
+                    Console.WriteLine($"Your result: {number1} * {number2} (Insert Drum roll here)= " + (number1 * number2));
+                    break;
+                case "d":
+                    Console.WriteLine($"Your result: {number1} / {number2} (Insert Drum roll here)= " + (number1 / number2));
+                    break;
+            }
+        }
+
+        public void MoreMathMagic()
+        {
             Console.WriteLine("Do you want more Number Magic");
             Console.WriteLine("\ty - Yes");
             Console.WriteLine("\tn - No");
@@ -27,38 +57,11 @@ namespace Calculator
             {
                 case "y":
                     Console.Clear();
-                    goto Begining;
+                    Main();
                     break;
                 case "n":
                     Environment.Exit(0);
                     break;
-            }
-
-
-            void Math()
-            {
-                Console.WriteLine("Please Choose an option and watch the magic happen:");
-                Console.WriteLine("\ta - Add");
-                Console.WriteLine("\ts - Subtract");
-                Console.WriteLine("\tm - Multiply");
-                Console.WriteLine("\td - Divide");
-                Console.Write("Your option?");
-
-                switch (Console.ReadLine())
-                {
-                    case "a":
-                        Console.WriteLine($"Your result: {number1} + {number2} (Insert Drum roll here)= " + (number1 + number2));
-                        break;
-                    case "s":
-                        Console.WriteLine($"Your result: {number1} - {number2} (Insert Drum roll here)= " + (number1 - number2));
-                        break;
-                    case "m":
-                        Console.WriteLine($"Your result: {number1} * {number2} (Insert Drum roll here)= " + (number1 * number2));
-                        break;
-                    case "d":
-                        Console.WriteLine($"Your result: {number1} / {number2} (Insert Drum roll here)= " + (number1 / number2));
-                        break;
-                }
             }
         }
     }
